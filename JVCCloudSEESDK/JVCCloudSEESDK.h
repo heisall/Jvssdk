f05b719967daee877e32d6e1b11412e11c43463c
Sunny <songkunpeng@jovision.com>
2015-12-11 16:49:32 +0800

    修改编译错误

--- JVCCloudSEESDK/JVCCloudSEESDK.h
+++ JVCCloudSEESDK/JVCCloudSEESDK.h
@@ -683,11 +683,10 @@ static NSString const *kWifiUserName     =  @"wifiUserName";
  */
 -(void)RemoteDownloadFile:(int)nLocalChannel withDownLoadPath:(char *)downloadPath withSavePath:(NSString *)SavePath;
 
- *  开启录像
+/*开启录像
  *
  *  @param nLocalChannel      连接的本地通道号
  *  @param saveLocalVideoPath 录像文件存放的地址
  */
 -(void)openRecordVideo:(int)nLocalChannel saveLocalVideoPath:(NSString *)saveLocalVideoPath;
->>>>>>> aa4f68b782b8c146a14f4118897911cdde684724
 @end

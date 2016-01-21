68e3aa84ccedad2bfdb078a8766928c0a14bf4c1
Joey <zhouye@jovision.com>
2016-01-21 19:18:56 +0800

    添加第三方报警的命令

--- JVCCloudSEESDK/JVCCloudSEESDK.h
+++ JVCCloudSEESDK/JVCCloudSEESDK.h
@@ -470,6 +470,26 @@ static NSString const *kWifiUserName     =  @"wifiUserName";
 -(void)RemoteRequestSendPlaybackVideo:(int)nLocalChannel withPlayBackPath:(NSString *)playBackVideoPath;
 
 /**
+ *  删除门磁和手环报警
+ *
+ *  @param nLocalChannel 本地连接通道号
+ *  @param alarmType     报警的类型
+ *  @param alarmGuid     报警的唯一标示
+ */
+-(void)RemoteDeleteDeviceAlarm:(int)nLocalChannel withAlarmType:(int)alarmType  withAlarmGuid:(int)alarmGuid;
+
+/**
+ *  编辑门磁和手环报警
+ *
+ *  @param nLocalChannel 本地连接通道号
+ *  @param alarmType     报警的类型
+ *  @param alarmGuid     报警的唯一标示
+ *  @param alarmEnable   报警是否开启
+ *  @param alarmName     报警的别名
+ */
+-(void)RemoteEditDeviceAlarm:(int)nLocalChannel withAlarmType:(int)alarmType  withAlarmGuid:(int)alarmGuid withAlarmEnable:(int)alarmEnable withAlarmName:(NSString *)alarmName;
+
+/**
  *  设置安全防护时间段
  *
  *  @param nLocalChannel  本地通道
